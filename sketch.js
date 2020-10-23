f1274611a28fcf4f61e01226146621a1ea8a48d6
Advik-star <71903276+Advik-star@users.noreply.github.com>
2020-10-22 23:10:01 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,33 +4,33 @@ var speed, weight;
 function setup() {
   createCanvas(800,400);
     
-  speed=random(55,150);
-  weight=random(1000,3000);
+  speed=random(55,90);
+  weight=random(1000,2500);
 
   car=createSprite(30,200,50,50);
   car.velocityX=speed;
-  car.shapeColor=(255,0,0);
+  car.shapeColor=color(255);
 
   wall=createSprite(700,200,80,height/2);
-  wall.shapeColor=(0,0,255);
+  wall.shapeColor=color(80,80,80);
   
 }
 
 function draw() {
-  background(100);
+  background(190);
 
   if(wall.x-car.x < (car.width+wall.width)/2){
     car.velocityX=0;
     var deformation=0.5 * weight * speed * speed/22500; 
 
     if(deformation > 180){
-      car.shapeColor=(255,0,0);
+      car.shapeColor=color(255,0,0);
     }
     if(deformation < 180 && deformation > 80){
-      car.shapeColor=(230,230,0);
+      car.shapeColor=color(230,230,0);
     }
     if(deformation < 80){
-      car.shapeColor=(0,255,0);
+      car.shapeColor=color(0,255,0);
     }
   }
   
